619ae73c7efb42f8f92eee12f48cf34055acb675
DonaldJNRO <okolocha.djnr.a@outlook.com>
2023-06-24 09:45:47 +0100

    initial commit

--- try.py
+++ try.py
@@ -3,29 +3,40 @@ import json
 import csv
 import matplotlib.pyplot as plt
 
+csv_file = ('bus-timetable.csv')
+
+infile = urllib.request.urlopen('https://api.tfl.gov.uk/StopPoint/490008645U/arrivals')
+
+
 class ArrivalTimes:
-    def __init__(self,infile):
-        self.infile = infile
+    def __init__(self, csv_file):
+        self._csv_file = csv_file
         self.arrival_times = []
 
     def fetch_arrival_times(self):
-        with urllib.request.urlopen(self.infile) as response:
-            arrival_times_json = response.read().decode()
-            self.arrival_times = json.loads(arrival_times_json)
+        arrival_times_json = infile.read().decode()
+        self.arrival_times = json.loads(arrival_times_json)
 
-    def save_to_csv(self, filename):
-        with open(filename, 'w', newline='') as csv_file:
+    def save_to_csv(self):
+        with open(self._csv_file, 'w', newline='') as csv_file:
             writer = csv.writer(csv_file)
-            writer.writerow(self.arrival_times[0].keys())
+            writer.writerow(self.arrival_times[0].keys())  # Write the header row
             for arrival in self.arrival_times:
                 writer.writerow(arrival.values())
         print("File saved successfully.")
+
+
 
     def display_arrival_stats(self):
         destinations = [arrival["destinationName"] for arrival in self.arrival_times]
         destination_counts = {destination: destinations.count(destination) for destination in set(destinations)}
         destinations = list(destination_counts.keys())
         counts = list(destination_counts.values())
+
+        with open(self._csv_file, 'r') as csv_file:
+            reader = csv.reader(csv_file)
+            next(reader)
+
 
         plt.bar(destinations, counts)
         plt.xlabel("Destination")
@@ -35,16 +46,18 @@ class ArrivalTimes:
         plt.tight_layout()
         plt.show()
 
-infile = 'https://api.tfl.gov.uk/StopPoint/490005624S/arrivals'
+    def get_csv_file(self):
+        return self._csv_file
 
-arrival_times = ArrivalTimes(infile)
+    def set_csv_file(self, csv_file):
+        self._csv_file = csv_file
+
+arrival_times = ArrivalTimes(csv_file)
 arrival_times.fetch_arrival_times()
 
 print(json.dumps(arrival_times.arrival_times, indent=4))
 
-csv_filename = "arrival_times.csv"
-arrival_times.save_to_csv(csv_filename)
+arrival_times.save_to_csv()
 
 arrival_times.display_arrival_stats()
-
 
